c965f78e5eedd2abf37a209bf6aca6b488aed893
mykobob <michael.eagle@gmail.com>
2014-10-04 20:36:07 -0700

    Working dropbox button. needs to call Jacob's URL

---
+++ DropboxSaver/getURL.js
@@ -0,0 +1,6 @@
+// var URL = "DropboxSaver/dropbox_icon.png";
+// var URL = "https://lh6.ggpht.com/fR_IJDfD1becp10IEaG2ly07WO4WW0LdZGUaNSrscqpgr9PI53D3Cp0yd2dXOgyux8w=w300";
+var URL = "https://www.moxtra.com/board/BRzaYwaCO2o6fyetppf8r1G/download?d=&type=pdf&t=CAEqBXAzUm1SehdCUnphWXdhQ08ybzZmeWV0cHBmOHIxR4ABBJADFA";
+
+
+document.getElementById("downloadButton").setAttribute("href", URL);
